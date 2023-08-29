cde1bb39a651fe2562035998458e4712a5070b40
qyx22122 <tibor.macek1@gmail.com>
2023-08-29 20:43:33 +0200

    Basic kernel

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Tibi_OS
 Simple OS writen in rust
+
+
+
+# Credit
+- https://os.phil-opp.com 
+	(AWESOME BLOG I FOLLOWED)
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,27 @@
-
 #![no_std]
 #![no_main]
 use core::panic::PanicInfo;
 
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> !{
+
+
+static HELLO: &[u8] = b"Hello World!";
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+     let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
 
 
-
-
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> !{
     loop {}
 }
