e6bc4d2fa6fcc3e7958c10d43a772d967cd473d0
qyx22122 <tibor.macek1@gmail.com>
2023-08-30 20:18:49 +0200

    Added print and println macros

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,27 @@
 #![no_std]
 #![no_main]
+
+
 use core::panic::PanicInfo;
 
 
+mod vga_buffer;
 
-static HELLO: &[u8] = b"Hello World!";
+
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-     let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    println!("Hello!");
+    panic!("hello error");
 
     loop {}
 }
 
 
+
+
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> !{
+fn panic(info: &PanicInfo) -> !{
+    print!("\n!!! {} !!!\n", info);
     loop {}
 }
