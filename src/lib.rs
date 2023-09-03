a1c5870c6c32dcbe96a4faee3f8dd8d87afddda0
qyx22122 <tibor.macek1@gmail.com>
2023-09-03 22:09:44 +0200

    Adding CPU interrupts

--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,14 @@
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
+#![feature(abi_x86_interrupt)]
 
 use core::panic::PanicInfo;
 
 
 pub mod serial;
 pub mod vga_buffer;
-
+pub mod interrupts;
 
 
 pub trait Testable {
@@ -73,4 +74,8 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
         let mut port = Port::new(0xf4);
         port.write(exit_code as u32);
     }
+}
+
+pub fn init(){
+    interrupts::init_idt();
 }
--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,25 @@
 #![test_runner(Tibi_OS::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+
+
 use core::panic::PanicInfo;
 use Tibi_OS::println;
 
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello!");
+    
+    for i in 0..100+1{
+       println!("Hello! {}",i); 
+    }
+    
+    
+    Tibi_OS::init();
+
+    x86_64::instructions::interrupts::int3();
+
+
     #[cfg(test)]
     test_main();
     
