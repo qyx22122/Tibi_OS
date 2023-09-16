f1801b127dea07cd447c24bd53ab1143ac58d89b
Nobody1902 <vitan.s.jaklic@gmail.com>
2023-09-16 19:44:56 +0200

    Removed tests, added Makefile and updated README.md

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,11 +1,6 @@
-
-
 use lazy_static::lazy_static;
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 use crate::println;
-
-
-
 
 
 lazy_static! {
@@ -23,10 +18,4 @@ pub fn init_idt(){
 
 extern "x86-interrupt" fn  breakpoint_handler(stack_frame: InterruptStackFrame){
 	println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
-}
-
-
-#[test_case]
-fn test_breakpoint_exception(){
-    x86_64::instructions::interrupts::int3();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,5 @@
 #![no_std]
 #![no_main]
-#![feature(custom_test_frameworks)]
-#![test_runner(Tibi_OS::test_runner)]
-#![reexport_test_harness_main = "test_main"]
-
-
 
 use core::panic::PanicInfo;
 use Tibi_OS::println;
@@ -20,30 +15,15 @@ pub extern "C" fn _start() -> ! {
     
     Tibi_OS::init();
 
-    x86_64::instructions::interrupts::int3();
-
-
-    #[cfg(test)]
-    test_main();
+    // x86_64::instructions::interrupts::int3();
     
     loop {}
 }
 
 
 //Panic handler
-#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> !{
     println!("\n!!! {} !!!", info);
     loop{}
 }
-
-//Panic handler in test
-
-#[cfg(test)]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    Tibi_OS::test_panic_handler(info)
-}
-
-
