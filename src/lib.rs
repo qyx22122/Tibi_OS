ab1d41c0943d398cd4477e2859caee4cd373913a
Nobody1902 <vitan.s.jaklic@gmail.com>
2023-10-29 11:23:48 +0100

    Clean up.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![feature(abi_x86_interrupt)]
 #![feature(const_mut_refs)]
+#![allow(non_snake_case)]
 
 extern crate alloc;
 
@@ -13,12 +14,30 @@ pub mod terminal;
 
 use crate::terminal::Terminal;
 
-pub fn init() {
+use bootloader::BootInfo;
+
+pub fn init(boot_info: &'static BootInfo) {
     gdt::init();
     let terminal: &Terminal = unsafe { &interrupts::init_idt() };
     unsafe { interrupts::PICS.lock().initialize() };
     x86_64::instructions::interrupts::enable();
+
+    setup_allocation(boot_info);
+
     terminal.init();
+}
+
+fn setup_allocation(boot_info: &'static BootInfo)
+{
+    use memory::BootInfoFrameAllocator;
+    use x86_64::VirtAddr;
+
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
+
+    allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
 }
 
 pub fn hlt_loop() -> ! {
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 #![no_std]
 #![no_main]
-
+#![allow(non_snake_case)]
 
 extern crate alloc;
-use alloc::string::*;
-use alloc::format;
-use alloc::{boxed::Box, rc::Rc, vec, vec::Vec};
+
+use alloc::string::String;
+
 use Tibi_OS::println;
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
@@ -13,47 +13,13 @@ use core::panic::PanicInfo;
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use Tibi_OS::allocator;
-    use Tibi_OS::memory::{self, BootInfoFrameAllocator};
-    use x86_64::VirtAddr;
 
     println!("Hello World{}", "!");
-    Tibi_OS::init();
+    Tibi_OS::init(boot_info);
 
-    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    
-    let mut mapper = unsafe { memory::init(phys_mem_offset) };
-    let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
+    let s = String::from("Testing");
 
-    allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
-
-    // allocate a number on the heap
-    let heap_value = Box::new(41);
-    println!("heap_value at {:p}", heap_value);
-
-    // create a dynamically sized vector
-    let mut vec = Vec::new();
-    for i in 0..500 {
-        vec.push(i);
-    }
-    println!("vec at {:p}", vec.as_slice());
-
-    // create a reference counted vector -> will be freed when count reaches 0
-    let reference_counted = Rc::new(vec![1, 2, 3]);
-    let cloned_reference = reference_counted.clone();
-    println!(
-        "current reference count is {}",
-        Rc::strong_count(&cloned_reference)
-    );
-    core::mem::drop(reference_counted);
-    println!(
-        "reference count is {} now",
-        Rc::strong_count(&cloned_reference)
-    );
-
-    let string = "Testing Strings".to_string();
-    let formated_string = format!("String: {}",string);
-    println!("{}",formated_string);
+    println!("String: {}", s);
 
     Tibi_OS::hlt_loop();
 }
